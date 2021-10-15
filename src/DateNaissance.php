54695898045437c86eb9eed44e7ec04963b0b1d5
messaouda <messaoudabenchikh@yahoo.fr>
2021-10-15 11:00:41 +0200

    créer la class DateNaissance.php et une class interface DateNaissanceInterface.php

---
+++ src/DateNaissance.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+use App\DateNaissanceInterface;
+
+class DateNaissance implements DateNaissanceInterface{
+
+    public function getJour()
+    {
+        
+    }
+    public function getMois()
+    {
+        
+    }
+    public function getAnnee()
+    {
+        
+    }
+}
---
+++ src/DateNaissanceInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace App;
+interface DateNaissanceInterface {
+    public function getJour();
+    public function getMois();
+    public function getAnnee();
+}
